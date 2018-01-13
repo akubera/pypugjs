5116f3099865bb782a20eecabc9c1442fd0a1d5f
Andy Grabow <andy@freilandkiwis.de>
2018-01-13 23:08:16 +0100

    just found out about the bad testing habits :/

--- pypugjs/testsuite/test_cases.py
+++ pypugjs/testsuite/test_cases.py
@@ -190,8 +190,8 @@ def run_case(case, process):
 
 
 exclusions = {
-    # its a pitty - the html compiler has the better results for mixins (indentation) but
-    # has to be excluded to not the other tests with their false results (bad expected indentation)
+    # its a pity - the html compiler has the better results for mixins (indentation) but
+    # has to be excluded to not "break" the other tests with their false results (bad expected indentation)
     'Html': {'mixins', 'mixin.blocks', 'layout', 'unicode', 'attrs.object', 'include_mixin'},
     'Mako': {'layout', 'include_mixin'},
     'Tornado': {'layout', 'include_mixin'},
