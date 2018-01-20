9c9ca378a6f8fc4c7154cf6dae49fd2657ee4097
Hsin-Cheng Chao <olala7846@gmail.com>
2018-01-20 10:11:07 +0800

    Use IOError in python 3 compatible way
    1. except IOError with `as ...` statement
    2. add TODO for this python 2 compatibility issue

--- pypugjs/ext/django/loader.py
+++ pypugjs/ext/django/loader.py
@@ -51,7 +51,8 @@ class Loader(BaseLoader):
                     contents = origin.loader.get_contents(origin)
                     contents = self.include_pug_sources(contents)
                     contents = process(contents, filename=origin.template_name, compiler=Compiler)
-                except IOError:
+                # TODO: Change IOError to FileNotFoundError after future==0.17.0
+                except IOError as err:
                     raise TemplateDoesNotExist(origin)
             else:
                 contents = origin.loader.get_contents(origin)
